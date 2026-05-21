426dff65b82fcc8bc3b2fea10b496cafcc9666ed
katealva <kate.alva@utec.edu.pe>
2026-05-20 21:27:33 -0500

    feat: creación de repositories (Closes #5)

---
+++ src/main/java/com/yala/tag/TagRepository.java
@@ -0,0 +1,13 @@
+package com.yala.tag;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByName(String name);
+
+    boolean existsByName(String name);
+}
