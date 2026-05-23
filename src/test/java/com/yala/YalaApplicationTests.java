9af2cddba03ca08763c7f7200e9b1a85329e5bb5
“Isracarlosz” <isracarlosz@gmail.com>
2026-05-23 18:01:37 -0500

    fix: forzar codificacion UTF-8 para arreglar error de maven-resources

--- src/test/java/com/yala/YalaApplicationTests.java
+++ src/test/java/com/yala/YalaApplicationTests.java
@@ -3,10 +3,16 @@ package com.yala;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class YalaApplicationTests {
+
+    // Esto engaña a Spring Boot haciéndole creer que S3 está conectado
+    @MockitoBean
+    private S3Client s3Client;
 
     @Test
     void contextLoads() {
