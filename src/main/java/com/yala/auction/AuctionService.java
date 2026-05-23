3151a6209631535b81098a83c9a7a69fba3414f2
sebas_ramos <royer.ramos@utec.edu.pe>
2026-05-23 16:45:12 -0500

    refactor(auction): mover endpoint y logica de pujas a BidService/BidController (SRP)

--- src/main/java/com/yala/auction/AuctionService.java
+++ src/main/java/com/yala/auction/AuctionService.java
@@ -3,8 +3,6 @@ package com.yala.auction;
 import com.yala.auction.dto.AuctionResponse;
 import com.yala.auction.dto.AuctionSummaryResponse;
 import com.yala.auction.dto.CreateAuctionRequest;
-import com.yala.bid.dto.BidResponse;
-import com.yala.bid.dto.CreateBidRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -15,8 +13,6 @@ public interface AuctionService {
     AuctionResponse findById(Long id);
 
     Page<AuctionSummaryResponse> findAllActive(Pageable pageable);
-
-    BidResponse placeBid(CreateBidRequest request, String bidderEmail);
 
     void closeExpiredAuctions();
 }
