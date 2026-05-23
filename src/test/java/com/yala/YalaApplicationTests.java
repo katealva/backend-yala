b987b4b089dd4476176307393b714ff6e4c3a0f6
“Isracarlosz” <isracarlosz@gmail.com>
2026-05-23 18:12:18 -0500

    test: inyectar propiedades mock de aws para arreglar contextLoads

--- src/test/java/com/yala/YalaApplicationTests.java
+++ src/test/java/com/yala/YalaApplicationTests.java
@@ -7,6 +7,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Import(TestcontainersConfiguration.class)
+@SpringBootTest(properties = {
+        "spring.cloud.aws.credentials.access-key=llave-falsa-de-prueba",
+        "spring.cloud.aws.credentials.secret-key=secreto-falso-de-prueba",
+        "spring.cloud.aws.region.static=us-east-1",
+        "aws.s3.bucket=bucket-de-prueba"
+})
+
 @SpringBootTest
 class YalaApplicationTests {
 
